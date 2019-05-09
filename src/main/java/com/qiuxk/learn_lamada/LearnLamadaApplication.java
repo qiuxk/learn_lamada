2c865e4bb7617ae728c02957aad4893e12056dc7
qiuxk <qxk1101@163.com>
2019-05-09 17:27:21 +0800

    Initial commit

---
+++ src/main/java/com/qiuxk/learn_lamada/LearnLamadaApplication.java
@@ -0,0 +1,13 @@
+package com.qiuxk.learn_lamada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnLamadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnLamadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qiuxk/learn_lamada/LearnLamadaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qiuxk.learn_lamada;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnLamadaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
